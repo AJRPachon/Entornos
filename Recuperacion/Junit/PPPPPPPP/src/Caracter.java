a981a1facfcd29463dab8b08ccbed7dc26e63952
Nzhdeh <nzhdeh1991@gmail.com>
2018-06-14 09:27:46 +0200

    Rec

---
+++ Recuperacion/Junit/PPPPPPPP/src/Caracter.java
@@ -0,0 +1,17 @@
+
+public class Caracter 
+{
+	private char car;
+	
+	public Caracter() 
+	{
+		car=' ';
+	}
+	
+	public Caracter(Caracter c) 
+	{
+		this.car=c.car;
+	}
+	
+	
+}
---
+++ Recuperacion/Junit/PPPPPPPP/src/TestCaracter.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCaracter 
+{
+	
+	Caracter c = new Caracter();
+	
+	int num=4;
+	boolean bul=true;
+	
+	@Test
+	public void testComprobarPalabra(int num,boolean bul)
+	{
+		//String
+	}
+
+}
